c229ec7eda2b07c7bafa4cf3a19c3fc906866aa6
clarkchen84 <sizhe.chen1@gmail.com>
2020-07-16 21:21:18 +0800

    add sub model

---
+++ spring-boot-demo/src/main/java/sizhe/chen/spring/boot/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package sizhe.chen.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName : DemoApplication
+ * @Description :
+ * @Author : Clark Chen
+ * @Date: 2020-07-16 21:01
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+}
